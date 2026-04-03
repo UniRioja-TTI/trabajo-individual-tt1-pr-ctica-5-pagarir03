06c89e6e42230b9f1582c37d7925f53599a98631
pagarir03 <paulagariijo@gmail.com>
2026-04-03 18:46:27 +0200

    terminado

--- src/main/java/com/tt1/trabajo/ConsumidorApi.java
+++ src/main/java/com/tt1/trabajo/ConsumidorApi.java
@@ -2,8 +2,7 @@ package com.tt1.trabajo;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+
 @Component
 public class ConsumidorApi {
 
@@ -11,18 +10,12 @@ public class ConsumidorApi {
     
     private final String URL_BASE = "http://localhost:9090/Resultados";
 
-   
-
     public String obtenerDatosGrid(String token) {
         try {
-            String url = "http://localhost:9090/Resultados";
-
-            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-            map.add("usuario", "paula");
-            map.add("tok", token);
-
-            return restTemplate.postForObject(url, map, String.class);
-
+            String finalUrl = "http://localhost:9090/Resultados?usuario=paula&tok=" + token;
+            
+            return restTemplate.postForObject(finalUrl, null, String.class);
+            
         } catch (Exception e) {
             return "Error al conectar con la MV: " + e.getMessage();
         }
